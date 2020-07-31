d72089bb4c8056478a560c3b9ae4964f1f59025f
Tim Eitel <timeitel@outlook.com>
2020-07-31 16:50:11 +0800

    Add passing data between routes

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,14 +9,17 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  String time = 'loading';
-
   setupWorldTime() async {
     WorldTime instance = WorldTime(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+
+    // request time from api
     await instance.getTime();
-    setState(() {
-      time = instance.time;
+
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
     });
   }
 
@@ -29,7 +32,7 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: EdgeInsets.all(50.0), child: Text(time)),
+      body: Padding(padding: EdgeInsets.all(50.0), child: Text('Loading')),
     );
   }
 }
